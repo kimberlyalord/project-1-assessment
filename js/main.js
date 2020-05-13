4a72c4e949ae70a90d9b7f6dc5fff1c19111642c
Kimberly Lord <kalord93@gmail.com>
2020-05-13 15:27:37 -0600

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,28 @@
+const addButton = document.getElementById('plus-button');
+const subtractButton = document.getElementById('minus-button');
+let input = document.querySelector('input');
+let counter = document.getElementById('counter');
+let total = 0;
+
+window.addEventListener('load', (event) => {
+    counter.innerHTML = `0`;
+    input.value = `1`;
+});
+
+addButton.addEventListener('click', addInteger);
+
+subtractButton.addEventListener('click', subtractInteger);
+
+function addInteger() {
+    total += parseInt(input.value);
+    counter.innerHTML = total;
+}
+
+function subtractInteger() {
+    total -= parseInt(input.value);
+    counter.innerHTML = total;
+}
+
+// if(total < 0) {   
+//     counter.innerHTML.style.color = "red";
+// }
